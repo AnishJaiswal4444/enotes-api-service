669ba4713cc860ff56bd83d97e1fdcb93074c6e6
AnishJaiswal4444 <anishjaiswal381@gmail.com>
2025-12-18 21:33:25 +0530

    added validation to category

--- src/main/java/Enotes_API_Service/exception/GlobalExceptionHandler.java
+++ src/main/java/Enotes_API_Service/exception/GlobalExceptionHandler.java
@@ -25,4 +25,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleResourceNotFoundException(Exception e){
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException e){
+        return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+    }
 }
---
+++ src/main/java/Enotes_API_Service/util/Validation.java
@@ -0,0 +1,52 @@
+package Enotes_API_Service.util;
+
+import Enotes_API_Service.Dto.CategoryDto;
+import Enotes_API_Service.exception.ValidationException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+public class Validation {
+    public void categoryValidation(CategoryDto categoryDto){
+
+        Map<String, Object> error  = new LinkedHashMap<>();
+        if (ObjectUtils.isEmpty(categoryDto))
+        {
+            throw new IllegalArgumentException("category object shouldn't be null or empty");
+        } else {
+
+            // validation name field
+            if(ObjectUtils.isEmpty(categoryDto.getName())){
+                error.put("name", "name field is empty or null");
+            }else{
+                if(categoryDto.getName().length() < 10){
+                    error.put("name", "name length is less then 10");
+                }
+                if(categoryDto.getName().length() > 100){
+                    error.put("name", "name length is greater then 100");
+                }
+            }
+
+            // validation description
+            if(ObjectUtils.isEmpty(categoryDto.getDescription())){
+                error.put("description", "description field is empty or null");
+            }
+
+            // validation isActive
+            if(ObjectUtils.isEmpty(categoryDto.getIsActive())){
+                error.put("isActive", "isActive field is empty or null");
+            }
+//            else{
+//                if(categoryDto.getIsActive() != Boolean.TRUE.booleanValue() && categoryDto.getIsActive() != Boolean.FALSE.booleanValue()){
+//                    error.put("isActive", "invalid value in isActive field");
+//                }
+//            }
+        }
+        if(!error.isEmpty()){
+            throw new ValidationException(error);
+        }
+    }
+}
