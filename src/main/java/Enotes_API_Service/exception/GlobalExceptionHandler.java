71ca05bb22e7798b6437a4df271ade763fadfc7b
AnishJaiswal4444 <anishjaiswal381@gmail.com>
2025-12-21 03:55:44 +0530

    added the GenericRespose Class that will handle the response that we will send from the API, till now we were using the inbuild ResponseEntity<> to showcase the response, However now we will use this class, then we created a Common util class and created 4 response methods, where we are using the GenericResponse class to to showcase the response then we called these methods by replacing all the API response in the COntroller class and by replacing all the error responses from the exception handler class, The benefit of using custom class is to make a respose that will be easly integrated by the frontend

--- src/main/java/Enotes_API_Service/exception/GlobalExceptionHandler.java
+++ src/main/java/Enotes_API_Service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package Enotes_API_Service.exception;
 
+import Enotes_API_Service.util.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +14,31 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<?> handleNullPointerException(Exception e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(Exception e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+//        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<?> handleValidationException(ValidationException e){
-        return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+//        return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+        return CommonUtil.createErrorResponse(e.getErrors(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ExistDataException.class)
     public ResponseEntity<?> handleExistDataException(ExistDataException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+//        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.CONFLICT);
     }
 }
